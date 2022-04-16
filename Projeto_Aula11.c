8cbbb584bc2826852d35d978a1994772dff80c4c
João O'Donnell <talaiajoao211@gmail.com>
2022-04-15 23:11:32 -0300

    atualização trabalho

--- Projeto_Aula11.c
+++ Projeto_Aula11.c
@@ -1,7 +1,3 @@
-#include <iostream>
-#include <string>
-#include <strings.h>
-using namespace std;
 /*
 objetivo da Aula:
 
@@ -13,186 +9,192 @@ objetivo da Aula:
         # notas A1, A2 e A3
 - Capacidade de excluir um aluno já cadastrado pela matrícula
 - Listar os alunos cadstrados com nome, email e matrícula
-- listar os alunos cadstrados com matrícula, nome, notas A1, A2 e A3, altém da média final
+- listar os alunos cadstrados com matrícula, nome, notas A1, A2 e A3, além da média final
 - A média é conposta de A1 valendo 20% da nota final, A2 30% da nota final e A3 valendo 50% da nota final;
 - Listar os alunos aprovados com matrícula, nome e média final. Aprovados tem média igual ou superior a 7;
 - Listar os alunos reprovados com matrícula, nome e média final. Aprovados tem média igual ou superior a 7;
 - Os percentuais de participação das notas podem sofrer mudanças futuramente, bem como a nota de aprovação;
 */
 
-typedef struct  {
-    //char nome[40];    
+#include <stdio.h>
+#include <strings.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+//Variável para mudar o máximo de alunos para cadastrar
+int maxAlunos = 3;
+
+//Struct para a o cadastro do aluno
+struct ficha_aluno{
+    
     char nome[50];
     char email[50];
-    int matricula = -1;
+    int matricula;
     float A1;
     float A2;
     float A3;
-} Aluno;
+    float media;
+    bool ativo;
+};
+    //Variável da Struct
+    struct ficha_aluno aluno[100];
 
-void limpaTela() {
-    printf("\e[1;1H\e[2J");
+
+//FUNÇÃO PRINCIPAL
+void main (){
+    
+    while(1){
+    //Switch case para escolher a opção do Menu
+    int opcaoEscolhida = MenuPrincipal();
+    switch (opcaoEscolhida){
+    case 1:
+        CadastrarAluno();
+        break;
+
+    case 2:
+        ListarAluno();
+        break;
+    
+    case 3:
+        ListarAlunoAprovados();
+        break;
+
+    case 4: 
+        ListarAlunoReprovados();
+        break;
+
+    case 5: 
+        ExcluirAluno();
+        break;
+
+    case 6:
+        exit(0);
+        break;
+
+    default:
+        printf("\n!!!Escolha umas das quatro opcoes!!!\n");
+        break;
+    }
+    
+    }   
 }
 
-int menuPrincipal() {
-    //limpaTela();
-    printf("Menu Principal\n");
-    printf("==============\n");
-    printf("1. cadastrar aluno\n");
-    printf("2. listar alunos\n");
-    printf("3. exlcuir aluno\n");
-    printf("0. sair\n\n");
+//Função do Menu Principal
+int MenuPrincipal(){
     
-    int opt = 0;
-    do {
-        printf("informe opcao desejada: ");
-        scanf("%i", &opt);
-    } while (opt < 0 || opt > 3);
-    
-    return opt;
+    printf("\n\n\n=============================");
+    printf("\n     ***MENU PRINCIPAL***");
+    printf("\n=============================\n");
+    printf("1. Cadastra Aluno\n");
+    printf("2. Listar Alunos\n");
+    printf("3. Listar Alunos aprovados\n");
+    printf("4. Listar Alunos reprovados\n");
+    printf("5. Excluir Aluno\n");
+    printf("6. Sair\n");
+
+    int opcao;
+
+        do{
+        printf("\nEscolha uma das opcoes: ");
+        scanf("%d", &opcao);
+    }while (opcao < 1 || opcao > 6);
+
+return opcao;
 }
 
-void gravaAluno(int posicao, Aluno alunos[100]) {
-        printf("(%d) Informe o nome: ", posicao);
-        // get up to buffer size - 1 characters (to account for NULL terminator)
-        //scanf("%s", alunos[posicao].nome);
-        char temp[50];
-        scanf("%s", temp);
+//Função para cadastro de aluno
+int CadastrarAluno(){
+for (int i = 0; i < maxAlunos; i++){
+
+        printf ("------------------------------------------------");
+
+        setbuf(stdin, 0);
+        printf("\nNome: ");
+        fgets(aluno[i].nome, 50, stdin);
+
+        printf("\nEmail: ");
+        scanf("%s", aluno[i].email);
+
+        printf("\nMatricula: ");
+        scanf("%d", &aluno[i].matricula);
+
+        printf("\nNota 1: ");
+        scanf("%f", &aluno[i].A1);
         
-        strcpy(alunos[posicao].nome,temp);
+        printf("\nNota 2: ");
+        scanf("%f", &aluno[i].A2);
+        
+        printf("\nNota 3: ");
+        scanf("%f", &aluno[i].A3);
 
-        printf("Informe o email: ");
-        // get up to buffer size - 1 characters (to account for NULL terminator)
-        scanf("%s", alunos[posicao].email);
-        printf("Informe o número da matrícula: ");
-        scanf("%i", &alunos[posicao].matricula);
-        printf("Informe a A1: ");
-        scanf("%f", &alunos[posicao].A1);
-        printf("Informe a A2: ");
-        scanf("%f", &alunos[posicao].A2);
-        printf("Informe a A3: ");
-        scanf("%f", &alunos[posicao].A3);
-        printf("---------\n");
-    
+        aluno[i].media = ((aluno[i].A1 * 2) + (aluno[i].A2 * 3) + (aluno[i].A3 * 5)) / 10;
+
+        aluno[i].ativo = true;
+    }
 }
 
-int listarAlunos(Aluno cadAlunos[]) {
-    for( int x = 0; x < 100; x++) {
-            printf("%d %i\n", x, cadAlunos[x].matricula);
-            if (cadAlunos[x].matricula != -1) {
-                printf("Nome: %s email: %s Matrícula: %d\n", cadAlunos[x].nome, 
-                cadAlunos[x].email, cadAlunos[x].matricula);
+//Função para gravar o aluno 
+int ListarAluno(){
+
+for (int i = 0; i < maxAlunos; i++){
+
+            if (aluno[i].ativo){
+            printf("\n------------------------------------");
+            printf("\nID do aluno: %d", i);
+            printf("\nNome: %s", aluno[i].nome);
+            printf("\nEmail: %s", aluno[i].email);
+            printf("\nMatricula: %d", aluno[i].matricula);
+            printf("\nNota A1: %.1f", aluno[i].A1);
+            printf("\nNota A2: %.1f", aluno[i].A2);
+            printf("\nNota A3: %.1f", aluno[i].A3);
+            printf("\nMedia das notas: %.1f", aluno[i].media);
             }
         }
-        return 0;
-}
-void zeraCadastro(Aluno alunos[]) {
-    for (int i = 0 ; i < 100; i++){
-        alunos[i].matricula = -1;
-    }
- }
-
-int primeiroDisponivel(int matr[10]) {
-    int ret = -1;
-    for (int i = 0 ; i < 10; i++){
-        if (matr[i] == -1) { 
-            ret = i;
-            break;
-        }
-    }
-    return ret;
 }
 
+//Função para ALUNOS APROVADOS
+int ListarAlunoAprovados(){
 
-
-int main(int argc, char const *argv[]) {
-/*
-    char nome[10][50]; 
-    char email[10][50];
-    int matricula[10];
-    float A1[10], A2[10], A3[10];
-        //zeraCadastro(matricula);
-*/
-// substitui por
-    Aluno cadAlunos[100];
-
-    zeraCadastro(cadAlunos);
-
-    //printf("\e[1;1H\e[2J");
-    limpaTela();
-    while (true) {
-        int menu  = menuPrincipal();
-        switch (menu) {
-            case 1: {
-                //printf("informe a posicao que quer gravar: 1 ~ 10 :");
-                int i = 0; //primeiroDisponivel(matricula);
-                //scanf("%i", &i);
-                //gravaAluno(i, nome, email, matricula, A1, A2, A3);
-                gravaAluno(i, cadAlunos);
-                
-                break;
+for (int i = 0; i < maxAlunos; i++){
+        if (aluno[i].ativo){
+            if (aluno[i].media >= 7){
+            printf("\n==============================");
+            printf("\n****ALUNOS APROVADOS***");
+            printf("\n==============================");
+            printf("\nNome: %s", aluno[i].nome);
+            printf("\nEmail: %s", aluno[i].email);
+            printf("\nMatricula: %d", aluno[i].matricula);
+            printf("\nMedia das notas: %.1f", aluno[i].media);
             }
-            case 2: {
-            listarAlunos(cadAlunos);
-            break;
-            }
-            
+         }
         }
-        
-        
+}
+
+//Função para ALUNOS REPROVADOS
+int ListarAlunoReprovados(){
+
+for (int i = 0; i < maxAlunos; i++){
+        if (aluno[i].ativo){
+            if (aluno[i].media < 7){
+            printf("\n==============================");
+            printf("\n****ALUNOS REPROVADOS***");
+            printf("\n==============================");
+            printf("\nNome: %s", aluno[i].nome);
+            printf("\nEmail: %s", aluno[i].email);
+            printf("\nMatricula: %d", aluno[i].matricula);
+            printf("\nMedia das notas: %.1f", aluno[i].media);
+            }
+        }
     }
-    //printf("Nome: %s\nemail: %s\nMatrícula: %d\n", nome[1], email[1], matricula[1]);
-    //printf("Nome: %s\nemail: %s\nMatrícula: %d\n", nome[2], email[2], matricula[2]);
+}
+
+void ExcluirAluno(){
+    int escolha;
+
+    printf("\nDigite o ID do aluno que deseja excluir: ");
+    scanf("%d", &escolha);
     
-
-    //limpar console
-    //printf("\e[1;1H\e[2J");
-
-    char name[20];
-
-    // get up to buffer size - 1 characters (to account for NULL terminator)
-    scanf("%19[^\n]", name);
-
+    aluno[escolha].ativo = false;
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-typedef struct  {
-    //char nome[40];    
-    char *nome;
-    int matricula;
-    int a1;
-    int a2;
-    int a3
-} Aluno;
-//strcpy(alu1.nome,"Jean Paul");
-*/
